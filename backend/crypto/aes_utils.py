2b90f433a7179cdfc96bb57156b035da8933c7cb
Kush Bajaria <122049095+kushbajaria@users.noreply.github.com>
2026-05-07 21:06:13 -0700

    implemented sending encrypted message over the network

--- backend/crypto/aes_utils.py
+++ backend/crypto/aes_utils.py
@@ -3,6 +3,8 @@ import json
 import base64
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from crypto.rsa_utils import rsa_sign
+from crypto.dsa_utils import dsa_sign
 
 # Encryption Function
 def encrypt(plaintext, session_key):
@@ -16,3 +18,28 @@ def decrypt(ciphertext, iv, session_key):
     aesgcm = AESGCM(session_key)
     plaintext = aesgcm.decrypt(iv, ciphertext, None)
     return plaintext.decode()
+
+def send_message(plaintext, session_key, private_key, sender, signature_algo):
+    ciphertext, iv = encrypt(plaintext, session_key)
+    
+    ct_b64 = base64.b64encode(ciphertext).decode()
+    iv_b64 = base64.b64encode(iv).decode()
+
+    if signature_algo == "RSA":
+        signature = rsa_sign(ciphertext, private_key)
+    elif signature_algo == "DSA":
+        signature = dsa_sign(ciphertext, private_key)
+    else:
+        raise ValueError("sig_algo must be RSA or DSA")
+
+    sig_b64 = base64.b64encode(signature).decode()
+
+    send_message = {
+        "ciphertext": ct_b64,
+        "iv": iv_b64,
+        "signature": sig_b64,
+        "signature_algo": signature_algo,
+        "sender": sender
+    }
+    
+    return send_message
