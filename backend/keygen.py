7949548be610f9016cdef8a847326dd85ee195f8
Eduardo Tostado <eduardotostado@csu.fullerton.edu>
2026-05-17 02:12:25 -0700

    Keygen file fixed

--- backend/keygen.py
+++ backend/keygen.py
@@ -1,10 +1,8 @@
 import sys
-import pathlib import Path
+import pathlib from Path
 
-KEYS_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(Path(__file__).resolve().parent))
-
-from crypto.rsa.utils import (
+# Import RSA/DSA functions
+from crypto.rsa_utils import (
     generate_keypair as rsa_generate_keypair,
     serialize_private_key as rsa_serialize_private,
     serialize_public_key as rsa_serialize_public
@@ -16,16 +14,21 @@ from crypto.dsa_utils import (
     serialize_public_key as dsa_serialize_public
 )
 
+KEYS_DIR = Path(__file__).resolve().parent.parent / "keys"
 KEYS_DIR.mkdir(exist_ok = True)
 
 ENTITIES = ["house", "player1", "player2"]
 
 
 for entity in ENTITIES:
-    priv, pub = rsa_generate_keypair()
-    open(KEYS_DIR / f"{entity}_rsa.pub", "wb").write(rsa_serialize_public(pub))
-    open(KEYS_DIR / f"{entity}_rsa.priv", "wb").write(rsa_serialize_public(priv))
+    # RSA keypair
+    rsa_priv, rsa_pub = rsa_generate_keypair()
+    with open(KEYS_DIR / f"{entity}_rsa.pub", "wb") as f:
+        f.write(rsa_serialize_public(rsa_pub))
+    with open(KEYS_DIR / f"{entity}_rsa.priv", "wb") as f:
+        write(rsa_serialize_private(rsa_priv))
 
-    priv, pub = dsa_generate_keypair()
+    # DSA keypair
+    dsa_priv, dsa_pub = dsa_generate_keypair()
     open(KEYS_DIR / f"{entity}_dsa.pub", "wb").write(dsa_serialize_public(pub))
-    open(KEYS_DIR / f"{entity}_dsa.priv", "wb").write(dsa_serialize_public(priv))
+    open(KEYS_DIR / f"{entity}_dsa.priv", "wb").write(dsa_serialize_priavte(priv))
